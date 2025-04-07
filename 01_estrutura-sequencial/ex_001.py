79daf221671b435cdef2c7961572039da962b7cc
yasminvsilva <alunoyasminsilva07@gmail.com>
2025-04-07 18:31:49 -0300

    s1 - exercício 001

---
+++ 01_estrutura-sequencial/ex_001.py
@@ -0,0 +1,13 @@
+"""
+Exercício 01 da seção de estrutura sequencial da Python Brasil:
+https://wiki.python.org.br/EstruturaSequencial
+
+Escreva um programa que mostre a mensagem:
+Olá, mundo!
+
+"""
+
+def ola_mundo():
+    print("Olá, mundo!")
+
+ola_mundo()
